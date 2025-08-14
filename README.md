bff598533f87f3ec5c006d3e3647086371a5bbe9
Daniel Martling <daniel.martling@hotmail.com>
2025-08-14 11:54:05 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Leaflet.NauticalScale
 Extension to the Leaflet Scale control that shows nautical miles.
+
+## What it does
+
+Adds Nautical Miles (1 NM = 1852 m) to the default Leaflet scale control. Divides nautical miles into tenths (0.1 NM) if necessary.
+
+![Scale control](img/screenshot3.png)
+
+## Install instructions
+
+1. Clone the repo.
+2. Copy the `leaflet.nauticalscale.js` to your project. 
+3. Include eg. `<script src="src/leaflet.nauticalscale.js"></script>` in the head of your html file.
+
+## Usage example
+
+Add the scale to your map:
+```
+const scale = new L.Control.ScaleNautical().addTo(map);
+```
+
+With some options:
+```
+const scale = new L.Control.ScaleNautical({
+    position: "bottomleft",
+    metric: true,
+    nautical: true,
+    maxWidth: 300
+}).addTo(map);
+```
+
+### Options table
+
+| Option           | Type      | Default      | Description                                                              |
+| ---------------- | --------- | ------------ | ------------------------------------------------------------------------ |
+| `position`       | `String`  | `bottomleft` | Position of the control on the map.                                      |
+| `metric`         | `Boolean` | `true`       | Show the metric scale (meters/kilometers).                               |
+| `imperial`       | `Boolean` | `false`      | Show the imperial scale (feet/miles).                                    |
+| `nautical`       | `Boolean` | `true`       | Show the nautical scale (NM).                                            |
+| `maxWidth`       | `Number`  | `100`        | Maximum width of the scale bar, in pixels.                               |
+| `updateWhenIdle` | `Boolean` | `false`      | If `true`, the control is updated only after the map has stopped moving. |
+
+
+## Screenshots
+|                                         |                                                                                                 |
+| --------------------------------------- | ----------------------------------------------------------------------------------------------- |
+| Nautical miles and kilometers           | ![Screenshot showing control with nautical miles and kilometers](img/screenshot1.png)           |
+| Tenths of nautical miles and kilometers | ![Screenshot showing control with tenths of nautical miles and kilometers](img/screenshot2.png) |
---
+++ leaflet.nauticalscale.js
@@ -0,0 +1,98 @@
+/*
+ *  Leaflet Nautical Scale
+ *  Version 1.0
+ *  Copyright (c) 2025 Daniel Martling
+ * 
+ *  A simple extension of the scale control that also shows nautical miles.
+ */
+L.Control.ScaleNautical = L.Control.Scale.extend({
+    options: {
+        position: "bottomleft",
+        nautical: true,
+        imperial: false,
+        metric: true,
+        maxWidth: 100,
+        updateWhenIdle: false
+    },
+
+    onAdd: function (map) {
+        var className = "leaflet-control-scale",
+            container = L.DomUtil.create('div', className);
+        var options = this.options;
+
+        this._addScales(className, container);
+
+        map.on(options.updateWhenIdle ? 'moveend' : 'move', this._update, this);
+        map.whenReady(this._update, this);
+
+        return container;
+    },
+
+    _addScales: function (className, container) {
+        if (this.options.metric) {
+            this._mScale = L.DomUtil.create('div', className + '-line', container);
+        }
+        if (this.options.imperial) {
+            this._iScale = L.DomUtil.create('div', className + '-line', container);
+        }
+        if (this.options.nautical) {
+            this._nScale = L.DomUtil.create('div', className + '-line', container);
+        }
+    },
+
+    _update: function () {
+        var map = this._map,
+            y = map.getSize().y / 2;
+
+        var maxMeters = map.distance(
+            map.containerPointToLatLng([0, y]),
+            map.containerPointToLatLng([this.options.maxWidth, y]));
+
+        this._updateScales(maxMeters);
+    },
+
+    _updateScales: function (maxMeters) {
+        if (this.options.metric && maxMeters) {
+            this._updateMetric(maxMeters);
+        }
+        if (this.options.imperial && maxMeters) {
+            this._updateImperial(maxMeters);
+        }
+        if (this.options.nautical && maxMeters) {
+            this._updateNautical(maxMeters);
+        }
+    },
+
+    _updateNautical: function (maxMeters) {
+        var maxCables = maxMeters / 185.2,
+            maxNauticalMiles,
+            cables,
+            nauticalMiles;
+
+        if (maxCables >= 10) {
+            maxNauticalMiles = maxMeters / 1852;
+            nauticalMiles = L.Control.Scale.prototype._getRoundNum.call(this, maxNauticalMiles);
+            this._updateScale(this._nScale, nauticalMiles + ' NM', nauticalMiles / maxNauticalMiles);
+        } else {
+            cables = L.Control.Scale.prototype._getRoundNum.call(this, maxCables);
+            this._updateScale(this._nScale, (cables / 10).toFixed(1) + ' NM', cables / maxCables);
+        }
+    },
+
+    _updateScale: function (scale, text, ratio) {
+        var width = Math.round(this.options.maxWidth * ratio),
+            screenWidth = this._map.getSize().x;
+        if (width > screenWidth) {
+            scale.style.display = "none";
+        } else {
+            scale.style.display = "";
+            scale.style.width = width + 'px';
+            scale.innerHTML = text;
+        }
+    }
+
+});
+
+L.control.scaleNautical = function (options) {
+    return new L.Control.ScaleNautical(options);
+};
